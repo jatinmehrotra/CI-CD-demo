63565b4fc88db9882aa93edd3609f073a5e91cc8
Jatin Mehrotra <jatinmehrotra@HL00796.local>
2021-10-06 17:18:03 +0530

    added credentials action

--- tests/index.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-describe('Test / route', () => {
-  test('should return 201', async () => {
-    await request(app).get('/').expect(200);
-  });
-});
--- tests/users.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-describe('Test /users route', () => {
-  test('should return 201', async () => {
-    await request(app).get('/users').expect(200);
-  });
-});
